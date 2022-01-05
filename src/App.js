53945552ac5e19d64c454ff89f44cf3f42601eeb
stevedsimkins <stevedsimkins@gmail.com>
2022-01-04 21:39:57 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, Suspense } from "react"
-import { useWallet } from "use-wallet";
-import { ethers } from "ethers";
-import web3 from "web3";
+import React, { useState, useEffect, Suspense } from 'react'
+import { useWallet } from 'use-wallet'
+import { ethers } from 'ethers'
+import web3 from 'web3'
 import {
   useColorMode,
   useToast,
@@ -20,146 +20,161 @@ import {
   Button,
   ButtonGroup,
   Spinner,
-} from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
-import { FaGithub, FaTwitter, } from "react-icons/fa";
-import avatar from "./assets/avatar.jpg";
+} from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
+import avatar from './assets/avatar.jpg'
 
-import abi from "./BuyMeCoffee.json";
-import { OWNER_ADDRESS, CONTRACT_ADDRESS } from "./constants";
+import abi from './BuyMeCoffee.json'
+import { OWNER_ADDRESS, CONTRACT_ADDRESS } from './constants'
 
-const contractABI = abi.abi;
+const contractABI = abi.abi
 
-const YOUR_NAME = "Steve";
-const USER_NAME = "stevedsimkins";
+const YOUR_NAME = 'Steve'
+const USER_NAME = 'stevedsimkins'
 
-const GITHUB_LINK = `https://github.com/${USER_NAME}`;
-const TWITTER_LINK = `https://twitter.com/${USER_NAME}`;
+const GITHUB_LINK = `https://github.com/${USER_NAME}`
+const TWITTER_LINK = `https://twitter.com/${USER_NAME}`
 
-const someAddress = "0x2Fd0BD0d1c846682F3730cB3F6c22052B43495A9";
+const someAddress = '0x2Fd0BD0d1c846682F3730cB3F6c22052B43495A9'
 
 function App() {
-  const toast = useToast();
+  const toast = useToast()
 
   //State
-  const [currentAccount, setCurrentAccount] = useState(null);
-  const [donationValue, setDonationValue] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [allDonations, setAllDonations] = useState([]);
-  const [message, setMessage] = useState("");
+  const [currentAccount, setCurrentAccount] = useState(null)
+  const [donationValue, setDonationValue] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [allDonations, setAllDonations] = useState([])
+  const [message, setMessage] = useState('')
 
   //use-wallet hook
-  const wallet = useWallet();
+  const wallet = useWallet()
 
   // Hook for changing from light mode to dark mode
-  const { colorMode, toggleColorMode } = useColorMode();
-  const bg = useColorModeValue("gray.200", "#2c313d");
-  const color = useColorModeValue("black", "white");
-  //Ethereum Functions 
+  const { colorMode, toggleColorMode } = useColorMode()
+  const bg = useColorModeValue('gray.200', '#2c313d')
+  const color = useColorModeValue('black', 'white')
+  //Ethereum Functions
   const checkAddress = async () => {
     try {
-      const result = web3.utils.checkAddressChecksum(someAddress);
+      const result = web3.utils.checkAddressChecksum(someAddress)
       console.log(result)
-    } catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
   const checkIfWalletIsConnected = async () => {
     try {
-      const { ethereum } = window;
-      getAllDonations();
+      const { ethereum } = window
+      getAllDonations()
       if (!ethereum) {
-        console.log("Make sure you have MetaMask!");
-        return;
+        console.log('Make sure you have MetaMask!')
+        return
       } else {
-        console.log("We have the ethereum object", ethereum);
+        console.log('We have the ethereum object', ethereum)
 
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        const accounts = await ethereum.request({ method: 'eth_accounts' })
 
         if (accounts.length !== 0) {
-          const account = accounts[0];
-          console.log('Found an authorized account:', account);
-          setCurrentAccount(account);
+          const account = accounts[0]
+          console.log('Found an authorized account:', account)
+          setCurrentAccount(account)
         } else {
-          console.log('No authorized account found');
+          console.log('No authorized account found')
         }
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const sendDonation = async () => {
     try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const buyMeCoffeeContract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const buyMeCoffeeContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        contractABI,
+        signer
+      )
 
-      let depositTxn = await buyMeCoffeeContract.deposit(`${donationValue}`, `${message}`, { value: ethers.utils.parseEther(donationValue) });
-      setIsLoading(true);
-      await depositTxn.wait();
-      setIsLoading(false);
-      setDonationValue("");
-      setMessage("");
-      toastPopUp();
-      getAllDonations();
+      let depositTxn = await buyMeCoffeeContract.deposit(
+        `${donationValue}`,
+        `${message}`,
+        { value: ethers.utils.parseEther(donationValue) }
+      )
+      setIsLoading(true)
+      await depositTxn.wait()
+      setIsLoading(false)
+      setDonationValue('')
+      setMessage('')
+      toastPopUp()
+      getAllDonations()
     } catch (error) {
       console.log(error)
     }
-  };
-
-  const withdraw = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const buyMeCoffeeContract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
-
-    let withdrawTxn = await buyMeCoffeeContract.withdraw();
-    setIsLoading(true);
-    await withdrawTxn.wait();
-    setIsLoading(false);
-    toastPopUpWithdraw();
   }
 
+  const withdraw = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const buyMeCoffeeContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      contractABI,
+      signer
+    )
+
+    let withdrawTxn = await buyMeCoffeeContract.withdraw()
+    setIsLoading(true)
+    await withdrawTxn.wait()
+    setIsLoading(false)
+    toastPopUpWithdraw()
+  }
 
   const connectWalletAction = async () => {
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       if (!ethereum) {
-        alert("Get Metamask!")
-        return;
+        alert('Get Metamask!')
+        return
       }
 
-      wallet.connect();
+      wallet.connect()
       setCurrentAccount(wallet.account)
-      checkNetwork();
+      checkNetwork()
     } catch (error) {
       console.log(error)
     }
-    getAllDonations();
+    getAllDonations()
   }
 
   const getAllDonations = async () => {
-    const { ethereum } = window;
+    const { ethereum } = window
 
     try {
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const buyMeCoffeeContract = new ethers.Contract(CONTRACT_ADDRESS, contractABI, signer);
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const buyMeCoffeeContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractABI,
+          signer
+        )
 
-        let donations = await buyMeCoffeeContract.getAllCoffees();
+        let donations = await buyMeCoffeeContract.getAllCoffees()
 
         let donationsCleaned = []
-        donations.forEach(donation => {
+        donations.forEach((donation) => {
           donationsCleaned.push({
             sender: donation.sender,
             amount: donation.amount,
             message: donation.message,
           })
-        });
-        donationsCleaned.reverse();
-        setAllDonations(donationsCleaned);
+        })
+        donationsCleaned.reverse()
+        setAllDonations(donationsCleaned)
       } else {
         console.log("ethereum object doesn't exist")
       }
@@ -167,32 +182,35 @@ function App() {
       console.log(error)
     }
   }
-  //Use Effect 
+  //Use Effect
   useEffect(() => {
     if (currentAccount) {
-      getAllDonations();
+      getAllDonations()
     }
-    checkAddress();
-    checkIfWalletIsConnected();
-  }, [currentAccount]);
+    checkAddress()
+    checkIfWalletIsConnected()
+  }, [currentAccount])
 
-  //utils 
+  //utils
   const numberInputHandler = (e) => {
-    setDonationValue(e.target.value);
+    setDonationValue(e.target.value)
   }
 
   const messageInputHandler = (e) => {
-    setMessage(e.target.value);
+    setMessage(e.target.value)
   }
 
   const shortenAddress = (address) => {
-    return `${address.slice(0, 4)}...${address.slice(address.length - 4, address.length)}`;
+    return `${address.slice(0, 4)}...${address.slice(
+      address.length - 4,
+      address.length
+    )}`
   }
 
   const checkNetwork = async () => {
     try {
       if (window.ethereum.networkVersion !== '4') {
-        alert("Please connect to Rinkeby!")
+        alert('Please connect to Rinkeby!')
       }
     } catch (error) {
       console.log(error)
@@ -201,29 +219,35 @@ function App() {
 
   const toastPopUp = () =>
     toast({
-      title: "Donation Sent!",
+      title: 'Donation Sent!',
       description: `Thank you for supporting ${YOUR_NAME}!`,
-      status: "success",
+      status: 'success',
       duration: 9000,
       isClosable: true,
     })
   const toastPopUpWithdraw = () =>
     toast({
-      title: "Withdraw Successful!",
+      title: 'Withdraw Successful!',
       description: `Please check your wallet to confirm the funds sent to your account`,
-      status: "success",
+      status: 'success',
       duration: 9000,
       isClosable: true,
     })
 
-
   //Conditional Rendering
   const renderContent = () => {
-    const { ethereum } = window;
+    const { ethereum } = window
     if (!ethereum) {
       return (
-        <Box display="flex" justifyContent="center" alignItems="center" width="full">
-          <a href="https://metamask.io/" target="_blank"><Button width="full">Download Metamask</Button></a>
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          width='full'
+        >
+          <a href='https://metamask.io/' target='_blank'>
+            <Button width='full'>Download Metamask</Button>
+          </a>
         </Box>
       )
     }
@@ -231,58 +255,113 @@ function App() {
     if (wallet.status === 'connected') {
       return (
         <VStack spacing={6} py={5}>
-          {wallet.account === OWNER_ADDRESS ? (
-            null
-          ) :
+          {wallet.account === OWNER_ADDRESS ? null : (
             <>
-              <NumberInput width="100%">
-                <NumberInputField onChange={numberInputHandler} value={donationValue} placeholder="Ξ" />
+              <NumberInput width='100%'>
+                <NumberInputField
+                  onChange={numberInputHandler}
+                  value={donationValue}
+                  placeholder='Ξ'
+                />
               </NumberInput>
-              <Input placeholder="Write a message!" onChange={messageInputHandler} value={message} />
+              <Input
+                placeholder='Write a message!'
+                onChange={messageInputHandler}
+                value={message}
+              />
             </>
-          }
-          {isLoading ? <Spinner /> :
-            <Button w="100%" colorScheme="blue" onClick={() => { wallet.account === OWNER_ADDRESS ? withdraw() : sendDonation() }} >{wallet.account === OWNER_ADDRESS ? `Withdraw` : `Send Ξ ${donationValue}`}</Button>
-          }
+          )}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <Button
+              w='100%'
+              colorScheme='blue'
+              onClick={() => {
+                wallet.account === OWNER_ADDRESS ? withdraw() : sendDonation()
+              }}
+            >
+              {wallet.account === OWNER_ADDRESS
+                ? `Withdraw`
+                : `Send Ξ ${donationValue}`}
+            </Button>
+          )}
         </VStack>
-
       )
     } else {
       return (
-        <Box display="flex" justifyContent="center" alignItems="center" width="full">
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          width='full'
+        >
           <Button onClick={() => connectWalletAction()}>Connect Wallet</Button>
         </Box>
       )
     }
   }
 
-
   return (
-    <Container maxW="container.lg" >
-      <IconButton pos="absolute" top="5%" right="5%" aria-label="toggle color mode" icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode} />
-      <Flex justify="center" align="center" minH="100vh" flexWrap="wrap" py={20} >
+    <Container maxW='container.lg'>
+      <IconButton
+        pos='absolute'
+        top='5%'
+        right='5%'
+        aria-label='toggle color mode'
+        icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        onClick={toggleColorMode}
+      />
+      <Flex
+        justify='center'
+        align='center'
+        minH='100vh'
+        flexWrap='wrap'
+        py={20}
+      >
         <Box mx={10}>
-          <Box display="flex" justifyContent="center" alignItems="center" w="full" py={3}>
-            <Avatar size="xl" name="Steve" src={avatar} />
+          <Box
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            w='full'
+            py={3}
+          >
+            <Avatar size='xl' name='Steve' src={avatar} />
           </Box>
           <Box py={3}>
             <Heading py={2}>Support with Crypto!</Heading>
             {wallet.account === OWNER_ADDRESS ? (
-              <Text fontSize="md">
-                Withdraw your donations below!
-              </Text>
+              <Text fontSize='md'>Withdraw your donations below!</Text>
             ) : (
-              <Text fontSize="md">
+              <Text fontSize='md'>
                 If you would like to support {YOUR_NAME} in his <br />
-                creative endeavors feel free to use this dApp <br /> to buy him a coffee with crypto! <br /> </Text>)} </Box> {renderContent()}
+                creative endeavors feel free to use this dApp <br /> to buy him
+                a coffee with crypto! <br />{' '}
+              </Text>
+            )}{' '}
+          </Box>{' '}
+          {renderContent()}
         </Box>
         <Suspense fallback={<Spinner />}>
-          <Box display="flex" justifyContent="center" alignItems="center" flexDir="column">
+          <Box
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            flexDir='column'
+          >
             <Heading> 🚀 {YOUR_NAME}'s Supporters</Heading>
-            <Box mx={10} my={3} overflowY="scroll" maxH="350">
+            <Box mx={10} my={3} overflowY='scroll' maxH='350'>
               {allDonations.map((donation, index) => {
                 return (
-                  <Box width={400} p={4} my={3} bg={bg} color={color} rounded="md" >
+                  <Box
+                    width={400}
+                    p={4}
+                    my={3}
+                    bg={bg}
+                    color={color}
+                    rounded='md'
+                  >
                     <Text>From: {shortenAddress(donation.sender)}</Text>
                     <Text>Donated: Ξ {donation.message}</Text>
                     <Text>Message: {donation.amount}</Text>
@@ -292,16 +371,27 @@ function App() {
             </Box>
           </Box>
         </Suspense>
-        <Box pos="absolute" bottom="2%">
-          <Text fontSize="sm" py={2}>Created by Steve Simkins</Text>
-          <ButtonGroup display="flex" justifyContent="center" alignItems="center" spacing="6">
-            <a target="_blank" href={GITHUB_LINK}><IconButton aria-label="github social" icon={<FaGithub />} /></a>
-            <a target="_blank" href={TWITTER_LINK}><IconButton aria-label="Twitter social" icon={<FaTwitter />} /></a>
+        <Box pos='absolute' bottom='2%'>
+          <Text fontSize='sm' py={2}>
+            Created by Steve Simkins
+          </Text>
+          <ButtonGroup
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            spacing='6'
+          >
+            <a target='_blank' href={GITHUB_LINK}>
+              <IconButton aria-label='github social' icon={<FaGithub />} />
+            </a>
+            <a target='_blank' href={TWITTER_LINK}>
+              <IconButton aria-label='Twitter social' icon={<FaTwitter />} />
+            </a>
           </ButtonGroup>
         </Box>
       </Flex>
-    </Container >
-  );
+    </Container>
+  )
 }
 
-export default App;
+export default App
